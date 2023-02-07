31d2fe16c980bc1f5a4aaeaf26c696e848783789
Ashok Raju <deville75@gmail.com>
2023-02-06 22:01:08 -0500

    Update install.sh for AshokYADR

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ ! -d "$HOME/.yadr" ]; then
-    echo "Installing YADR for the first time"
-    git clone --depth=1 https://github.com/skwp/dotfiles.git "$HOME/.yadr"
+    echo "Installing AshokYADR for the first time"
+    git clone --depth=1 https://github.com/rajuashok/dotfiles.git "$HOME/.yadr"
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
     rake install
 else
-    echo "YADR is already installed"
+    echo "AshokYADR is already installed"
 fi
